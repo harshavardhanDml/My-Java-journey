c4b6fa406bb5f163d020586bef446572f9d92a34
harshavardhan <harshavardhan052007@gmail.com>
2026-09-20 17:20:58 +0530

    Function added

---
+++ Func.java
@@ -0,0 +1,9 @@
+public class Func {
+    static void Mymethod(){
+        System.out.println("It is my function method");
+    }
+    
+    public static void main(String[] args){
+        Mymethod();
+    }
+}
