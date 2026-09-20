bbdf7395fba97452b2408d3329493769c744add2
harshavardhan <harshavardhan052007@gmail.com>
2026-09-20 15:57:15 +0530

    Arrays in java

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args){
+        String[] names={"harsha","vinay","kiran","sai"};
+        // System.out.println(names[0]);
+        // System.out.println(names[1]);
+        // System.out.println(names[2]);
+
+        //for(int i=0;i<names.length;i++){
+           // System.out.println(names[i]);
+        //}
+        names[3]="vijay";
+        System.out.println(names[3]);
+
+    }
+}
+
